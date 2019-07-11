77cd085db6508ba007ac14d646171e93a318e10e
ekrusznis <ekrusznis@aol.com>
2019-07-11 09:15:45 -0400

    Initial commit

---
+++ app/src/main/java/dvelopd/kotlintutorial/MainActivity.kt
@@ -0,0 +1,64 @@
+package dvelopd.kotlintutorial
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBar
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mDrawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        mDrawerLayout = findViewById(R.id.drawerLayout)
+        val navigationView: NavigationView = findViewById(R.id.navView)
+
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.mipmap.ic_launcher)
+        }
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            mDrawerLayout.closeDrawers()
+
+            when (menuItem.itemId) {
+                R.id.option1 -> {
+                    Toast.makeText(this, "Profile", Toast.LENGTH_LONG).show()
+                }
+                R.id.option2 -> {
+                    Toast.makeText(this, "Wallet", Toast.LENGTH_LONG).show()
+                }
+                R.id.option3 -> {
+                    Toast.makeText(this, "Offer", Toast.LENGTH_LONG).show()
+                }
+            }
+            true
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+
+}
